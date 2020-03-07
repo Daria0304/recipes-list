ccf6b0caea172a0f83dd3eb7944bc682e405b6c1
Daria Szubierajska <d.szubierajska@gmail.com>
2020-03-07 18:36:24 -0300

    First setup

---
+++ src/tables/RecipeTable.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const RecipeTable = props => (
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Ingredients</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      {props.recipes.length > 0 ? (
+        props.recipes.map(recipe => (
+          <tr key={recipe.id}>
+            <td>{recipe.name}</td>
+            <td>{recipe.ingredient}</td>
+            <td>
+              <button className="button muted-button">Edit</button>
+              <button className="button muted-button">Delete</button>
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={3}>No recipes</td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+)
+
+export default RecipeTable
