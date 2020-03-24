c90dac8c68eb7b3556ec57979bec25aa8d7ac9bb
Daria Szubierajska <d.szubierajska@gmail.com>
2020-03-24 19:59:31 -0300

    CRUD done.Still need to fixthe code

---
+++ src/forms/EditRecipeForm.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+
+const EditRecipeForm = props => {
+  const [recipe, setRecipe] = useState(props.currentRecipe)
+
+  useEffect(() => {
+    setRecipe(props.currentRecipe)
+    }, [props])
+
+  const handleInputChange = event => {
+    const { name, value } = event.targer
+
+    setRecipe({ ...recipe, [name]: value })
+  }
+
+  return (
+    <form
+      onSubmit={event => {
+        event.preventDefault()
+
+        props.updateRecipe(recipe.id, recipe)
+      }}
+    >
+      <label>Name</label>
+      <input type="text" name="name" value={recipe.name} onChange={handleInputChange} />
+      <label>Ingredients</label>
+      <input type="text" name="ingredient" value={recipe.ingredient} onChange={handleInputChange} />
+      <button>Update Recipe</button>
+      <button onClink={() => props.setEditing(false)} className="button muted-button">
+      Cancel
+      </button>
+    </form>
+  )
+}
+
+export default EditRecipeForm
